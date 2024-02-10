52bb4ec1b63dc3666e598274d11ae1aeb9ac0a6e
Derek Boucher <derek.boucher@me.com>
2024-02-09 19:42:55 -0500

    cleaned up nvim plugins

--- .config/nvim/lua/plugins/git-linker.lua
+++
@@ -1,3 +0,0 @@
-return {
-	{ "pabloariasal/webify.nvim" },
-}
--- .config/nvim/lua/plugins/workspaces.lua
+++ .config/nvim/lua/plugins/workspaces.lua
@@ -4,14 +4,6 @@ return {
 		config = function()
 			local workspaces = require("workspaces")
 			workspaces.setup()
-
-			vim.keymap.set("n", "<leader>af", function()
-				workspaces.add(vim.fn.input("Path to add: "))
-			end)
-			-- vim.keymap.set("n", "<C-r>", workspaces.list_dirs, {})
-			vim.keymap.set("n", "<leader>aw", function()
-				workspaces.add(vim.fn.getcwd())
-			end)
 		end,
 	},
 }
