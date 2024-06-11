b3fea1d45a50f5efe72b8fe9c0a03feb9bf0e55a
Derek Boucher <derek.boucher@me.com>
2024-06-11 01:57:31 -0400

    moar

--- .config/sway/custom_bar.sh
+++ .config/sway/custom_bar.sh
@@ -5,8 +5,9 @@ do
     docker_running_containers=$(docker ps -q | wc -l)
     docker_version=$(docker --version | awk '{print $3}' | sed 's/,//g')
     docker_images=$(docker images -q | wc -l)
+    current_volume_sink=$(pacmd list-sinks | grep -A 40 '*' | grep 'alsa.card_name' | sed 's/.*alsa.card_name = "\(.*\)"/\1/')
 
     display_protocol=$(echo $XDG_SESSION_TYPE)
 
-    echo "$(whoami) | $display_protocol | Docker 🐳 v$docker_version Running Containers: $docker_running_containers, Images: $docker_images" || exit 1
+    echo "🎧 $(current_volume_sink) | $(whoami) | $display_protocol | Docker 🐳 v$docker_version Running Containers: $docker_running_containers, Images: $docker_images" || exit 1
 done
