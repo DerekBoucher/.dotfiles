df3f1f715dc36376cb6c65c9537eacfac0344c6b
Derek Boucher <derek.boucher@me.com>
2024-06-11 20:45:16 -0400

    updates to sway

--- .config/sway/custom_bar.sh
+++ .config/sway/custom_bar.sh
@@ -6,8 +6,9 @@ do
     docker_version=$(docker --version | awk '{print $3}' | sed 's/,//g')
     docker_images=$(docker images -q | wc -l)
     current_volume_sink=$(pacmd list-sinks | grep -A 40 '*' | grep 'alsa.card_name' | sed 's/.*alsa.card_name = "\(.*\)"/\1/')
-
     display_protocol=$(echo $XDG_SESSION_TYPE)
+    power_profile=$(asusctl profile -p | awk 'END {print $NF}')
+    
 
-    echo "🎧 $(current_volume_sink) | $(whoami) | $display_protocol | Docker 🐳 v$docker_version Running Containers: $docker_running_containers, Images: $docker_images" || exit 1
+    echo "🔌 $power_profile | 🎧 $current_volume_sink | 👋 $(whoami) | $display_protocol | Docker 🐳 v$docker_version 📦 Containers: $docker_running_containers, Images: $docker_images" || exit 1
 done
