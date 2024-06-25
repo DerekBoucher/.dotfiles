3e39de28dbf4cd23f4bc0e8df28c1d6b46bc8c11
Derek Boucher <derek.boucher@me.com>
2024-06-24 21:21:55 -0400

    disabled vim visual multi

--- .config/nvim/lua/plugins/visual-vim-multi.lua
+++ .config/nvim/lua/plugins/visual-vim-multi.lua
@@ -1,15 +1,16 @@
 return {
-	{
-		"mg979/vim-visual-multi",
-		lazy = false,
-		init = function()
-			vim.g.VM_Maps = nil
-			vim.g.VM_maps = {
-				["Find Under"] = "<C-l>",
-				["Find Subword Under"] = "<C-l>",
-				["Select Cursor Down"] = "<M-C-Down>",
-				["Select Cursor Up"] = "<M-C-Up>",
-			}
-		end,
-	},
+	-- {
+	-- 	"mg979/vim-visual-multi",
+	-- 	lazy = false,
+	-- 	init = function()
+	-- 		vim.g.VM_Maps = nil
+	-- 		vim.g.VM_maps = {
+	-- 			["Find Under"] = "<C-l>",
+	-- 			["Find Subword Under"] = "<C-l>",
+	-- 			["Move Right"] = "",
+	-- 			["Move Left"] = "",
+	-- 			["Toggle Mappings"] = "<leader>lmm",
+	-- 		}
+	-- 	end,
+	-- },
 }
