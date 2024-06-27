091398dc72326fbd59006b0d125507a7594f3773
Derek Boucher <derek.boucher@me.com>
2024-06-27 16:21:54 -0400

    neotree shows hidden files

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,16 @@ return {
 		},
 		config = function()
 			require("nvim-tree").setup({
+				filters = {
+					enable = true,
+					git_ignored = false,
+					dotfiles = false,
+					git_clean = false,
+					no_buffer = false,
+					no_bookmark = false,
+					custom = {},
+					exclude = {},
+				},
 
 				update_focused_file = {
 					enable = true,
