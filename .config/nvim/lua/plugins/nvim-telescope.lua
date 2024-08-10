600c35ba631fd10396078409ac12200dde736463
Derek Boucher <derek.boucher@me.com>
2024-08-09 20:30:40 -0400

    made git meta files searchable in telescope

--- .config/nvim/lua/plugins/nvim-telescope.lua
+++ .config/nvim/lua/plugins/nvim-telescope.lua
@@ -1,40 +1,40 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			local telescope = require("telescope")
-			telescope.setup({
-				pickers = {
-					find_files = {
-						hidden = true,
-						file_ignore_patterns = { "node_modules/", "^.git/*", "target/" },
-						find_command = { "rg", "--ignore", "--hidden", "--files", "-u" },
-					},
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							--- opts
-						}),
-					},
-					["workspaces"] = {
-						-- opts
-					},
-					["emoji"] = {
-						action = function(emoji)
-							vim.api.nvim_put({ emoji.value }, "c", false, true)
-						end,
-					},
-				},
-			})
-			telescope.load_extension("ui-select")
-			telescope.load_extension("workspaces")
-			telescope.load_extension("dap")
-			telescope.load_extension("emoji")
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "xiyaowong/telescope-emoji.nvim" },
-	},
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                        file_ignore_patterns = { "node_modules/", "^.git/", "target/" },
+                        find_command = { "rg", "--ignore", "--hidden", "--files", "-u" },
+                    },
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({
+                            --- opts
+                        }),
+                    },
+                    ["workspaces"] = {
+                        -- opts
+                    },
+                    ["emoji"] = {
+                        action = function(emoji)
+                            vim.api.nvim_put({ emoji.value }, "c", false, true)
+                        end,
+                    },
+                },
+            })
+            telescope.load_extension("ui-select")
+            telescope.load_extension("workspaces")
+            telescope.load_extension("dap")
+            telescope.load_extension("emoji")
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "xiyaowong/telescope-emoji.nvim" },
+    },
 }
