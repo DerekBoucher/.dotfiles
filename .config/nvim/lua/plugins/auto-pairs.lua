0d343f9d1bc6c7c5256e249c50b9fe1430d2d5bc
Derek Boucher <derek.a.boucher@gmail.com>
2024-10-14 23:21:14 -0400

    moar updates

--- .config/nvim/lua/plugins/auto-pairs.lua
+++ .config/nvim/lua/plugins/auto-pairs.lua
@@ -1,15 +1,5 @@
 return {
-    {
-        "jiangmiao/auto-pairs",
-    },
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end,
-    },
+	{
+		"jiangmiao/auto-pairs",
+	},
 }
